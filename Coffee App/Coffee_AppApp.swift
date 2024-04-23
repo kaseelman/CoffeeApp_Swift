ba2736efef92638b42ea6fc7e214f07f479e9b11
Kas Eelman <kas.eelman@gmail.com>
2024-04-23 15:20:31 +0200

    Initial Commit

---
+++ Coffee App/Coffee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_AppApp.swift
+//  Coffee App
+//
+//  Created by Kas Eelman on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
